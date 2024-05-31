511f21fa59ef2f820911bddddcf8aa5d25178d68
jesusmaylle@gmail.com <jesusmaylle@gmail.com>
2024-05-30 19:28:31 -0500

    Agregar archivos de proyecto.

---
+++ G_11/NumberListClass1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_11
+{
+    internal class NumberList
+    {
+        private List<int> numbers;
+
+        public NumberList()
+        {
+            numbers = new List<int>();
+        }
+
+        public void AddNumber(int number)
+        {
+            numbers.Add(number);
+        }
+
+        public List<int> GetNumbers()
+        {
+            return numbers;
+        }
+
+        public void RemoveNumber(int number)
+        {
+            numbers.Remove(number);
+        }
+
+        public int SequentialSearch(int number)
+        {
+            return SequentialSearchRecursive(number, 0);
+        }
+
+        private int SequentialSearchRecursive(int number, int index)
+        {
+            if (index >= numbers.Count)
+                return -1; // No encontrado
+            if (numbers[index] == number)
+                return index;
+            return SequentialSearchRecursive(number, index + 1);
+        }
+
+        public void QuickSort()
+        {
+            QuickSortRecursive(0, numbers.Count - 1);
+        }
+
+        private void QuickSortRecursive(int left, int right)
+        {
+            if (left < right)
+            {
+                int pivotIndex = Partition(left, right);
+                QuickSortRecursive(left, pivotIndex - 1);
+                QuickSortRecursive(pivotIndex + 1, right);
+            }
+        }
+
+        private int Partition(int left, int right)
+        {
+            int pivot = numbers[right];
+            int i = left - 1;
+            for (int j = left; j < right; j++)
+            {
+                if (numbers[j] <= pivot)
+                {
+                    i++;
+                    Swap(i, j);
+                }
+            }
+            Swap(i + 1, right);
+            return i + 1;
+        }
+
+        private void Swap(int a, int b)
+        {
+            int temp = numbers[a];
+            numbers[a] = numbers[b];
+            numbers[b] = temp;
+        }
+
+        public void SortAscending()
+        {
+            QuickSort();
+        }
+
+        public void SortDescending()
+        {
+            QuickSort();
+            numbers.Reverse();
+        }
+
+        public void PrintNumbers()
+        {
+            foreach (var number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ G_11/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberList numberList = new NumberList();
+            bool exit = false;
+
+            while (!exit)
+            {
+                Console.Clear();
+                Console.WriteLine("1. Insertar número");
+                Console.WriteLine("2. Ordenar de forma ascendente");
+                Console.WriteLine("3. Ordenar de forma descendente");
+                Console.WriteLine("4. Buscar un número");
+                Console.WriteLine("5. Eliminar un número");
+                Console.WriteLine("6. Mostrar lista");
+                Console.WriteLine("7. Salir");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Console.Clear();
+                        Console.Write("Ingrese el número a insertar: ");
+                        int numberToAdd = int.Parse(Console.ReadLine());
+                        numberList.AddNumber(numberToAdd);
+                        break;
+                    case "2":
+                        Console.Clear();
+                        numberList.SortAscending();
+                        Console.WriteLine("Lista ordenada de forma ascendente.");
+                        numberList.PrintNumbers();
+                        break;
+                    case "3":
+                        Console.Clear();
+                        numberList.SortDescending();
+                        Console.WriteLine("Lista ordenada de forma descendente.");
+                        numberList.PrintNumbers();
+                        break;
+                    case "4":
+                        Console.Clear();
+                        Console.Write("Ingrese el número a buscar: ");
+                        int numberToSearch = int.Parse(Console.ReadLine());
+                        int index = numberList.SequentialSearch(numberToSearch);
+                        if (index != -1)
+                            Console.WriteLine($"Número encontrado en la posición {index}.");
+                        else
+                            Console.WriteLine("Número no encontrado.");
+                        break;
+                    case "5":
+                        Console.Clear();
+                        Console.Write("Ingrese el número a eliminar: ");
+                        int numberToRemove = int.Parse(Console.ReadLine());
+                        numberList.RemoveNumber(numberToRemove);
+                        Console.WriteLine("Número eliminado. Lista actualizada:");
+                        numberList.PrintNumbers();
+                        break;
+                    case "6":
+                        Console.Clear();
+                        Console.WriteLine("Lista actual:");
+                        numberList.PrintNumbers();
+                        break;
+                    case "7":
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Opción no válida. Intente de nuevo.");
+                        break;
+                }
+                Console.ReadKey();
+            }
+            
+        }
+    }
+    
+}
